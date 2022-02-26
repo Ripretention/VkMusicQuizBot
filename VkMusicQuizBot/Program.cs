41486e730f1868d948804afd0e1d5d1a2eba297b
Ripretention <r5r44e@gmail.com>
2022-02-26 23:46:37 +0300

    Add DI

--- VkMusicQuizBot/Program.cs
+++ VkMusicQuizBot/Program.cs
@@ -3,6 +3,7 @@ using System;
 using VkNetLongpoll;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace VkMusicQuizBot
 {
@@ -10,33 +11,44 @@ namespace VkMusicQuizBot
     {
         public static async Task Main(string[] args)
         {
-            var cfg = new BotConfigurationBuilder().Build().Get<BotConfiguration>();
-            var db = new FileDatabase(cfg.Database.PathFolder);
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            using var servicesProvider = services.BuildServiceProvider();
 
-            var downloader = new AudioTrackDownloader(cfg.FFMpeg);
-
-            var spotifyAuth = new SpotifyAuth(cfg.Spotify.Auth);
-            var spotify = new SpotifyClient(new SpotifyAPI(spotifyAuth));
-
-            var vkApi = new VkApi();
-            await vkApi.AuthorizeAsync(new VkNet.Model.ApiAuthParams { AccessToken = cfg.Vk.AccessToken });
-            var longpoll = new Longpoll(vkApi, (long)cfg.Vk.GroupId);
-
-            var admCommands = new AdministrationCommands(longpoll.Handler, db, cfg.Developers);
-            var commonCommands = new CommonCommands(
-                longpoll.Handler, 
-                db, 
-                new SpotifyAudioTrackExtractor(spotify, cfg.Spotify.PlaylistSourceId), 
-                downloader
-            );
-            var newMessageHandler = new NewMessageHandler(longpoll.Handler);
-            admCommands.Release();
+            var amdCommands = servicesProvider.GetService<AdministrationCommands>();
+            var commonCommands = servicesProvider.GetService<CommonCommands>();
+            var newMessageHandler = servicesProvider.GetService<NewMessageHandler>();
+            amdCommands.Release();
             commonCommands.Release();
             newMessageHandler.Release();
 
+            var longpoll = servicesProvider.GetService<Longpoll>();
             Console.WriteLine("Longpoll has been started");
 
             await longpoll.Start();
+        }
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(p => new BotConfigurationBuilder().Build().Get<BotConfiguration>());
+            services.AddSingleton<IFileDatabase>(p => new FileDatabase(p.GetService<BotConfiguration>().Database.PathFolder));
+            services.AddSingleton<IAudioTrackDownloader>(p => new AudioTrackDownloader(p.GetService<BotConfiguration>().FFMpeg));
+            services.AddSingleton<VkNet.Abstractions.IVkApi>(p =>
+            {
+                var vkApi = new VkApi();
+                vkApi.Authorize(new VkNet.Model.ApiAuthParams { AccessToken = p.GetService<BotConfiguration>().Vk.AccessToken });
+                return vkApi;
+            });
+            services.AddSingleton(p => new Longpoll(p.GetService<VkNet.Abstractions.IVkApi>(), (long)p.GetService<BotConfiguration>().Vk.GroupId));
+            services.AddSingleton(p => p.GetService<Longpoll>().Handler);
+            services.AddSingleton<IAudioTrackExtractor>(p =>
+            {
+                var spotifyAuth = new SpotifyAuth(p.GetService<BotConfiguration>().Spotify.Auth);
+                var spotify = new SpotifyClient(new SpotifyAPI(spotifyAuth));
+                return new SpotifyAudioTrackExtractor(spotify, p.GetService<BotConfiguration>().Spotify.PlaylistSourceId);
+            });
+            services.AddSingleton<AdministrationCommands>();
+            services.AddSingleton<CommonCommands>();
+            services.AddSingleton<NewMessageHandler>();
         }
     }
 }
